608bbdc9493a7179a7483930f79883fe64f2049e
Rachna Tiwari <rachna@codestra.digital>
2025-04-04 23:45:49 +0530

    checking updates

--- my-app/app/students/page.tsx
+++ my-app/app/students/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function StudentsPage() {
     return (
         <div>
             <h1>Students Page</h1>
             <p>Welcome to the Students Page!</p>
+            
         </div>
     );
 }
---
+++ my-app/app/students/studentsName/[studentId]/feedback/page.tsx
@@ -0,0 +1,8 @@
+export default function FeedbackPage() {
+    return (
+        <div>
+            <h1>Student Feedback</h1>
+            <p>This is the feedback page for a specific student.</p>
+        </div>
+    );
+}
