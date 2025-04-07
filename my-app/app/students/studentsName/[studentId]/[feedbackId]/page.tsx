6ccba0a3e9b5f849b11450ee80fbb59d1fbb6552
Rachna Tiwari <rachna@codestra.digital>
2025-04-07 18:12:50 +0530

    removed unwanted code

--- my-app/app/students/studentsName/[studentId]/[feedbackId]/page.tsx
+++ my-app/app/students/studentsName/[studentId]/[feedbackId]/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function FeedbackIdPage({ params }: { params: { studentId: string; feedbackId:string } }) {
     return (
         <div>
-            <h1>Feedback Page</h1>
+            <h1>FeedbackId Page</h1>
             <p>studentID{params.studentId} and feedbackID {params.feedbackId} for students</p>
             
-            {/* <Link href="/students/studentsName/studentId/feedbackId"> students feedback for particular ID</Link> */}
+            
         </div>
     );
 }
